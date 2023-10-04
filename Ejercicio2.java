c279c98f97f1079f274b719031505e82f70c0497
aantoniolorenzoo <acanjim2704@g.educaand.es>
2023-10-04 12:43:25 +0200

    Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main (String[] args){
+        System.out.println("Antonio Lorenzo Cano Jiménez");
+        System.out.println("Calle Juan de Anchieta, 58");
+        System.out.println("663 64 14 42");
+    }
+}
