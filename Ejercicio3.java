c48a7320a5a951ccd6bdc132e9f12b65d2c8c5c0
aantoniolorenzoo <acanjim2704@g.educaand.es>
2023-10-05 09:56:37 +0200

    ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3 {
+    public static void main (String[] args) {
+        System.out.printf("%-10s%-10s\n", "Almeja","Clam");
+        System.out.printf("%-10s%-10s\n", "Anchoa","Anchovy");
+        System.out.printf("%-10s%-10s\n", "Besugo","Sea bream");
+        System.out.printf("%-10s%-10s\n", "Cangrejo","Crab");
+        System.out.printf("%-10s%-10s\n", "Gamba","Prawn");
+        System.out.printf("%-10s%-10s\n", "Lubina","Sea bass");
+        System.out.printf("%-10s%-10s\n", "Pulpo","Octopus");
+        System.out.printf("%-10s%-10s\n", "Mero","Grouper");
+        System.out.printf("%-10s%-10s\n", "Rape","Angler fish");
+        System.out.printf("%-10s%-10s\n", "Rodaballo","Turbot");
+    }
+}
